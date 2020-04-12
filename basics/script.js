8d4f76d3f7653e7cb437f2de3ce261e2cc7f97cc
Pedram Safaei <psafaei@nevada.unr.edu>
2020-04-11 23:32:31 -0700

    done with datatype and variables

--- basics/script.js
+++ basics/script.js
@@ -4,4 +4,7 @@ var firstName = "Pedram";
 var lastName = "Safaei";
 var age = 22;
 
+var fullAge = true;
+
 console.log(firstName + " " + lastName + " is " + age + " years old");
+console.log(fullAge);
