088b0d95ea5801b4516d6c7de3b10623c7558004
pedramsafaei <psafaei@nevada.unr.edu>
2020-05-04 15:55:49 -0700

    coding challenge2 added

---
+++ basics/truthyFalsy.js
@@ -0,0 +1,17 @@
+// falsy values: undefined, null, 0, '', NaN
+// truthy values: NOT falsy values
+
+var height;
+
+height = 23;
+
+if (height || height === 0) {
+  console.log("Variable is defined");
+} else {
+  console.log("Variable has NOT been defined");
+}
+
+// Equality operators
+if (height === "23") {
+  console.log("The == operator does type coercion!");
+}
